c0a59429905fa1a6fc47d5c74cf2848e628f53c7
nelsonhernandez20 <nelsonhernandez335@gmail.com>
2024-02-19 20:33:47 -0500

    modifications were made

--- test/components/previews/accordion_component_preview.rb
+++ test/components/previews/accordion_component_preview.rb
@@ -17,41 +17,7 @@ class AccordionComponentPreview < ViewComponent::Preview
   def item_component
     # render Accordion::ItemComponent.new(open: "false") do |component|
     #   component.with_trigger do
-        
-    #     render(Accordion::DefaultTriggerComponent.new) { "Title" }
-    #   end
-    #   # render Accordion::TriggerComponent.new do
-    #   # end
-    # end
-  end
 
-  def trigger
-  end
-
-  def default_trigger
-  end
-
-  def content_preview
-  end
-
-  def default_content
-  end
-
-  def default1
-    render Accordion::BuilderComponent.new do |accordion|
-      accordion.add_item("Item 1", "Content 1")
-      accordion.add_item("Item 2", "Content 2")
-      accordion.add_item("Item 3", "Content 3")
-    end
-  end
-
-  def builder_component
-  end
-
-  def item_component
-    # render Accordion::ItemComponent.new(open: "false") do |component|
-    #   component.with_trigger do
-        
     #     render(Accordion::DefaultTriggerComponent.new) { "Title" }
     #   end
     #   # render Accordion::TriggerComponent.new do
