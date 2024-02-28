919bb811d51f83762aa62b9bf7711a3451501394
nelsonhernandez20 <nelsonhernandez335@gmail.com>
2024-02-27 22:47:08 -0500

    changes in test

--- test/components/alert_component_test.rb
+++ test/components/alert_component_test.rb
@@ -3,17 +3,17 @@ require "test_helper"
 class BuilderAlertComponentTest < ViewComponent::TestCase
   test "renders warning alert with custom classes" do
     component = render_inline(Alert::BuilderAlertComponent.new(variant: :warning, class: "custom-class", classTitle: "custom-title-class", classDescription: "custom-description-class", color: "custom-color")) do |component|
-      component.with_title { "Título de alerta" }
-      component.with_description { "Descripción de alerta" }
-      component.with_icon { "Icono de alerta" }
+      component.with_title { "Title alert" }
+      component.with_description { "Description of Alert" }
+      component.with_icon { "Icon" }
     end
 
     assert_selector(".custom-class")
     assert_selector(".custom-title-class")
     assert_selector(".custom-description-class")
     assert_selector(".custom-color")
-    assert_text("Título de alerta")
-    assert_text("Descripción de alerta")
-    assert_text("Icono de alerta")
+    assert_text("Title alert")
+    assert_text("Description of Alert")
+    assert_text("Icon")
   end
 end
