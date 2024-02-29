e240c102ba48ad7e881a21507233992cd1ea7b70
nelsonhernandez20 <nelsonhernandez335@gmail.com>
2024-02-29 14:11:52 -0500

    new test for badgeComponent

--- test/components/badge_component_test.rb
+++ test/components/badge_component_test.rb
@@ -1,12 +1,17 @@
 require "test_helper"
 
 class BadgeComponentTest < ViewComponent::TestCase
+  def test_primary_variant
+    render_inline(BadgeComponent.new(variant: :primary, size: :md)) { "Hello, World!" }
+
+    assert_text("Hello, World!")
+  end
+
   def test_custom_class_overrides_default
     render_inline(BadgeComponent.new(variant: :primary, size: :md, class: "custom-class")) { "Hello, World!" }
 
     assert_text("Hello, World!")
 
     assert_selector(".custom-class")
-
   end
 end
