4f7dbb13d0821ddf49b5189a6a6408eda33366d9
nelsonhernandez20 <nelsonhernandez335@gmail.com>
2024-02-29 15:56:34 -0500

    changes format

--- test/components/previews/theme_toggle_component_preview.rb
+++ test/components/previews/theme_toggle_component_preview.rb
@@ -4,12 +4,12 @@ class ThemeToggleComponentPreview < ViewComponent::Preview
     render(ThemeToggleComponent.new) do |component|
       component.with_light do
         "<button data-action='click->theme-toggle#toggle' data-theme-toggle-target='lightButton' class='theme-toggle-button'>
-          Modo claro
+          Dark light
         </button>".html_safe
       end
       component.with_dark do
         "<button data-action='click->theme-toggle#toggle' data-theme-toggle-target='darkButton' class='theme-toggle-button'>
-          Modo oscuro
+          Dark Mode
         </button>".html_safe
       end
     end
