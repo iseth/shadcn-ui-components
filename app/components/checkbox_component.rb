7d5831724d44310802fa350d665d158472075481
nelsonhernandez20 <nelsonhernandez335@gmail.com>
2024-03-11 11:17:07 -0500

    crated checkbox

--- app/components/checkbox_component.rb
+++ app/components/checkbox_component.rb
@@ -1,8 +1,10 @@
 class CheckboxComponent < ViewComponent::Base
-  def initialize(id:, label:, checked: false, description: nil)
-    @id = id
-    @checked = checked
-    @label = label
-    @description = description
+  def initialize(**attrs)
+    base_classes = "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 accent-primary"
+    @attrs = attrs
+    classes = "#{base_classes} #{@attrs[:class]}"
+    @attrs[:class] = classes
+    @other_attrs = @attrs.except(:class, :checked).map { |k, v| "#{k}=\"#{v}\"" }.join(" ")
+    @checked = @attrs[:checked] ? "checked" : ""
   end
 end
