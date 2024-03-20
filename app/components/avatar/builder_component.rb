62bd0e5c0b325bb7a4b264d1d4e33760d1faebae
Nelson Hernandez <69705950+nelsonhernandez20@users.noreply.github.com>
2024-03-20 12:34:24 -0500

    26 Avatar Component (#28)
    * created component
    
    * fix avatar
    
    * fix avatar

---
+++ app/components/avatar/builder_component.rb
@@ -0,0 +1,12 @@
+class Avatar::BuilderComponent < ViewComponent::Base
+  def initialize(size: :md, src: nil, alt: nil, initials: nil, **attrs)
+    base_classes = "relative flex shrink-0 overflow-hidden rounded-full"
+    @attrs = attrs
+    @size = size
+    @src = src
+    @alt = alt
+    @initials = initials
+    classes = "#{base_classes} #{@attrs[:class]}"
+    @attrs[:class] = classes
+  end
+end
---
+++ app/components/avatar/image_component.rb
@@ -0,0 +1,10 @@
+class Avatar::ImageComponent < ViewComponent::Base
+  def initialize(src:, alt: "", **attrs)
+    base_classes = "aspect-square h-full w-full"
+    @attrs = attrs
+    @src = src
+    @alt = alt
+    classes = "#{base_classes} #{@attrs[:class]}"
+    @attrs[:class] = classes
+  end
+end
