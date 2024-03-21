4b8c2cd7e059a44f6aa56265280d2519331a2b35
nelsonhernandez20 <nelsonhernandez335@gmail.com>
2024-03-20 19:45:47 -0500

    removed files phlex

--- test/components/previews/phlex_checkbox_component_preview.rb
+++
@@ -1,9 +0,0 @@
-class PhlexCheckboxComponentPreview < ViewComponent::Preview
-  def default
-    render PhlexCheckboxComponent.new(id: "terms", checked: false, label: "Accept terms and conditions")
-  end
-
-  def checkbox_checked
-    render PhlexCheckboxComponent.new(id: "terms1", checked: true, label: "Accept terms and conditions", description: "You agree to our Terms of Service and Privacy Policy.")
-  end
-end
